1c989a8c4e8545377f3aac1e99d25b136e7df59b
yuvath <13.bscs.147@gmail.com>
2024-07-18 02:02:09 +0500

    Adding Classes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
+import AppLayout from "./Pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+
         <Route path="product" element={<Product />} />
-        <Route path="/" element={<Homepage />} />
-        <Route path="/" element={<Homepage />} />
+
         <Route path="pricing" element={<Pricing />} />
+
+        <Route path="app" element={<AppLayout />} />
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
