ba157d9c1d4891ee0e11146613f0280595def03c
yuvath <13.bscs.147@gmail.com>
2024-07-17 20:39:49 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+import Pricing from "./pages/Pricing";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
