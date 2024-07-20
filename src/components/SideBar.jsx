db17f83f0b26fde9df034969bf314aec1546f3b9
yuvath <13.bscs.147@gmail.com>
2024-07-20 21:30:11 +0500

    Add SideBar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,16 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import Footer from "./Footer";
+import styles from "./SideBar.module.css";
+
+function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <Footer />
+    </div>
+  );
+}
+
+export default SideBar;
