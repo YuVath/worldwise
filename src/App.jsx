6364c4f5a133f21cede6b7a2d7788cbcf3fbc757
yuvath <13.bscs.147@gmail.com>
2024-07-21 23:06:45 +0500

    Add CityList

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import AppLayout from "./Pages/AppLayout";
 import Login from "./Pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
@@ -17,8 +18,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
 
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of Cities</p>} />
-          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
