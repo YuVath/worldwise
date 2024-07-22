21a340683d2b91c2433f6d948c884ba677f64153
yuvath <13.bscs.147@gmail.com>
2024-07-22 05:21:32 +0500

    Add Validation

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,32 @@ import Pricing from "./pages/Pricing";
 import AppLayout from "./Pages/AppLayout";
 import Login from "./Pages/Login";
 import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -18,8 +42,14 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
 
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,34 @@
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner.jsx";
+import CityItem from "./CityItem.jsx";
+import Message from "./Message.jsx";
+import PropTypes from "prop-types";
 
-function CityList() {
-  return <ul className={styles.cityList}>LIST</ul>;
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
+
+CityList.propTypes = {
+  cities: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      // Add other city properties here, e.g. name, population, etc.
+    })
+  ).isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
 
 export default CityList;
